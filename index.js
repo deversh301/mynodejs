328f1b08628a12dd559afaf9cb78f60c21e4ddc6
Shubham <ershyadav303@gmail.com>
2026-04-08 17:28:43 +0530

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/docker", (req, res) => {
-  res.send("wao we have setup one pipeline shubham our first pipeline with AWS");
+  res.send("wao we have setup one pipeline  our first pipeline with AWS");
 });
 
 app.listen(5000, () => {
